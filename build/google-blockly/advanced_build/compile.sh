da5988320802b58a7e530b4ef6dfbbbd213f3b82
Martin Večeřa <marvenec@gmail.com>
2019-02-05 08:59:34 +0100

    Updated compilation

--- build/google-blockly/advanced_build/compile.sh
+++ build/google-blockly/advanced_build/compile.sh
@@ -9,13 +9,13 @@ java -jar closure-compiler.jar --js='main.js' \
   --js='../../closure-library/third_party/closure/goog/**.js' \
   --generate_exports \
   --externs ../externs/svg-externs.js \
-  --compilation_level ADVANCED_OPTIMIZATIONS \
+  --compilation_level SIMPLE_OPTIMIZATIONS \
   --dependency_mode=STRICT --entry_point=BlocklyMain \
   --js_output_file blockly.js
 
 cp blockly.js blockly.orig.js
 
-sed -i '' 's/window\.addEventListener("load",/window.Blockly.init=/g' blockly.js
-sed -i '' "s|</xml>'})});|</xml>'})};|g" blockly.js
+#sed -i '' 's/window\.addEventListener("load",/window.Blockly.init=/g' blockly.js
+#sed -i '' "s|</xml>'})});|</xml>'})};|g" blockly.js
 
 uglifyjs blockly.js -o blockly.min.js
