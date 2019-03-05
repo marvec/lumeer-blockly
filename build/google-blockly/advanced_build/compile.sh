42cae57f1caa74379f68e066f0d3b89163bf8ebe
Martin Večeřa <marvenec@gmail.com>
2019-03-05 12:11:35 +0100

    Updated to multilang

--- build/google-blockly/advanced_build/compile.sh
+++ build/google-blockly/advanced_build/compile.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-java -jar closure-compiler.jar --js='main.js' \
+java -jar closure-compiler.jar --js='main.cs.js' \
+  --js='../blocks/**.js' \
+  --js='../core/**.js' \
+  --js='../generators/**.js' \
+  --js='../msg/js/cs.js' \
+  --js='../../closure-library/closure/goog/**.js' \
+  --js='../../closure-library/third_party/closure/goog/**.js' \
+  --generate_exports \
+  --externs ../externs/svg-externs.js \
+  --compilation_level SIMPLE_OPTIMIZATIONS \
+  --dependency_mode=STRICT --entry_point=BlocklyMain \
+  --js_output_file blockly.cs.js
+
+cp blockly.cs.js blockly.cs.orig.js
+uglifyjs blockly.cs.js -o blockly.cs.min.js
+
+java -jar closure-compiler.jar --js='main.en.js' \
   --js='../blocks/**.js' \
   --js='../core/**.js' \
   --js='../generators/**.js' \
@@ -11,11 +27,7 @@ java -jar closure-compiler.jar --js='main.js' \
   --externs ../externs/svg-externs.js \
   --compilation_level SIMPLE_OPTIMIZATIONS \
   --dependency_mode=STRICT --entry_point=BlocklyMain \
-  --js_output_file blockly.js
+  --js_output_file blockly.en.js
 
-cp blockly.js blockly.orig.js
-
-#sed -i '' 's/window\.addEventListener("load",/window.Blockly.init=/g' blockly.js
-#sed -i '' "s|</xml>'})});|</xml>'})};|g" blockly.js
-
-uglifyjs blockly.js -o blockly.min.js
+cp blockly.en.js blockly.en.orig.js
+uglifyjs blockly.en.js -o blockly.en.min.js
---
+++ build/google-blockly/advanced_build/main.cs.js
@@ -0,0 +1,33 @@
+goog.provide('BlocklyMain');
+// Messages (in some language)
+goog.require('Blockly.Msg.cs');
+// Core
+goog.require('Blockly');
+// Blocks
+goog.require('Blockly.Constants.Logic');
+goog.require('Blockly.Constants.Loops');
+goog.require('Blockly.Constants.Math');
+goog.require('Blockly.Constants.Text');
+goog.require('Blockly.Constants.Lists');
+goog.require('Blockly.Constants.Variables');
+goog.require('Blockly.Constants.VariablesDynamic');
+goog.require('Blockly.Blocks.procedures');
+goog.require('Blockly.JavaScript');
+goog.require('Blockly.JavaScript.lists');
+goog.require('Blockly.JavaScript.logic');
+goog.require('Blockly.JavaScript.loops');
+goog.require('Blockly.JavaScript.math');
+goog.require('Blockly.JavaScript.procedures');
+goog.require('Blockly.JavaScript.texts');
+goog.require('Blockly.JavaScript.variablesDynamic');
+goog.require('Blockly.JavaScript.variables');
+goog.require('Blockly.FieldFA');
+
+Blockly.init = function(id = '', toolbox) {
+  Blockly.HSV_VALUE = 0.85;
+  return Blockly.inject('blocklyDiv'+id, {
+    'toolbox': toolbox
+  });
+};
+
+//window.addEventListener('load', BlocklyMain.init);
